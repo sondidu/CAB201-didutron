a3cf27ca4fa76d0db5cee63095a080596cbba95f
Sean Dewantoro <seanandrewvalentino@gmail.com>
2024-05-20 01:00:40 +1000

    Delete CommandValidation
    DON'T NEED THAT SHIT NO MORE.

--- CommandValidation/ArgType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CommandValidation
-{
-    public abstract class ArgType
-    {
-        public string argumentErrorMsg { get; protected set; } = "There is an invalid argument.";
-        public abstract bool isValid(string argument);
-    }
-}
