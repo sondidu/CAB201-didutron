a06931e0bb0b8b9d1f2cf4d15ff55f688848c80d
Sean Dewantoro <seanandrewvalentino@gmail.com>
2024-05-22 01:54:19 +1000

    Add XML comments on CommandTree

--- CommandTree/Command.cs
+++ CommandTree/Command.cs
@@ -1,21 +1,47 @@
 ﻿using ConstantsAndHelpers;
+
 namespace CommandTree
 {
+    /// <summary>
+    /// Represents a command node in a command tree.
+    /// </summary>
     public class Command
     {
+        /// <summary>
+        /// The children of the command.
+        /// </summary>
         public Dictionary<string, Command>? Children { get; }
 
+        /// <summary>
+        /// The action to be executed.
+        /// </summary>
         public Action<string[]> Execute { get; } = ErrorMessage.EmptyExecute;
 
+        /// <summary>
+        /// The error message of an unspecified command key.
+        /// </summary>
         public string? UnspecifiedCommandKeyMsg { get; }
 
+        /// <summary>
+        /// The error message of an invalid command key.
+        /// </summary>
         public string? InvalidCommandKeyMsg { get; }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Command"/> class where this command is a leaf.
+        /// </summary>
+        /// <param name="execute">The action to be executed.</param>
         public Command(Action<string[]> execute)
         {
             Execute = execute;
         }
 
+        /// <summary>
+        /// Initialises a new instace of the <see cref="Command"/> class where this command has children.
+        /// </summary>
+        /// <param name="children">The child commands.</param>
+        /// <param name="invalidCommandKeyMsg">The error message of an invalid command key.</param>
+        /// <param name="unspecifiedCommandKeyMsg">The error message of an unspecified command key.</param>
         public Command(Dictionary<string, Command> children, string invalidCommandKeyMsg = ErrorMessage.InvalidCommandKey, string unspecifiedCommandKeyMsg = ErrorMessage.UnspecifiedCommandKey)
         {
             Children = children;
--- CommandTree/Runner.cs
+++ CommandTree/Runner.cs
@@ -1,53 +1,73 @@
 ﻿using ConstantsAndHelpers;
 using CustomExceptions;
+
 namespace CommandTree
 {
+    /// <summary>
+    /// Runner class that executes commands.
+    /// </summary>
     public class Runner
     {
         private readonly Command Commands;
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Runner"/> class.
+        /// </summary>
+        /// <param name="commands">The commands to be executed.</param>
         public Runner(Command commands)
         {
             Commands = commands;
         }
 
+        /// <summary>
+        /// Executes the given input command.
+        /// </summary>
+        /// <param name="input">The input command to be executed.</param>
+        /// <exception cref="UnspecifiedCommandKeyException">Thrown when the command key is not specified.</exception>
+        /// <exception cref="InvalidCommandKeyException">Thrown when the command key is invalid.</exception>
         private void Run(string input)
         {
             Command commandIterator = Commands;
             string[] parts = input.Split(' ');
-            int wordIdx = 0;
+            int partIdx = 0;
 
             try
             {
                 // Iterate command tree until it reaches a command leaf
                 while (commandIterator.Children != null)
                 {
-                    string currentArg = parts[wordIdx];
+                    string currentArg = parts[partIdx];
                     commandIterator = commandIterator.Children[currentArg];
-                    wordIdx++;
+                    partIdx++;
                 }
             }
-            catch(IndexOutOfRangeException ex)
+            catch(IndexOutOfRangeException ex) // Caught when partIdx > parts.Length
             {
                 throw new UnspecifiedCommandKeyException(commandIterator.UnspecifiedCommandKeyMsg, ex);
             }
-            catch(KeyNotFoundException ex)
+            catch(KeyNotFoundException ex) // Caught when currentArg is not in commandIterator.Children.Keys
             {
-                throw new InvalidCommandKeyException(commandIterator.InvalidCommandKeyMsg, parts[wordIdx], ex);
+                throw new InvalidCommandKeyException(commandIterator.InvalidCommandKeyMsg, parts[partIdx], ex);
             }
 
-            // Copying the arguments in `parts` to `args`
-            int argCount = parts.Length - wordIdx;
+            // Copying the arguments in parts to args
+            int argCount = parts.Length - partIdx;
             string[] args = new string[argCount];
-            Array.Copy(parts, wordIdx, args, 0, argCount);
+            Array.Copy(parts, partIdx, args, 0, argCount);
 
             Action<string[]> execute = commandIterator.Execute;
             execute(args);
         }
 
+        /// <summary>
+        /// Tries to execute the given input command and returns a message if unsuccessful.
+        /// </summary>
+        /// <param name="input">The input command to be executed.</param>
+        /// <param name="message">The output message if the command execution is unsuccessful.</param>
+        /// <returns><c>true</c> if the command execution is successful; otherwise, <c>false</c>.</returns>
         public bool TryRun(string? input, out string message)
         {
-            input ??= "";
+            input ??= ""; // If null, then set to empty string
             try
             {
                 Run(input);
@@ -72,7 +92,7 @@ namespace CommandTree
                 message = ex.Message;
                 return false;
             }
-            catch(Exception ex)
+            catch(Exception ex) // Unspecified error caught
             {
                 message = ex.Message;
                 return false;
